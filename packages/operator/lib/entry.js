b04cfcbce6c7c462a3eb346bb0c4f849eebf71a1
satellitex <s.a.t.e.3.ths@gmail.com>
2019-05-09 03:19:33 +0900

    WIP: laod types.jspm

--- packages/operator/lib/entry.js
+++ packages/operator/lib/entry.js
@@ -1,4 +1,13 @@
+const { ApiPromise } = require('@polkadot/api');
+const fs = require('fs');
+
 async function main() {
+    var typeList = JSON.parse(fs.readFileSync("./../../types.json"));
+
+    const api = await ApiPromise.create({
+        types: typeList
+    });
+
     console.log('boot main!')
 }
 
