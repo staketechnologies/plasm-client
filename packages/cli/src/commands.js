48220e2356258c6dbb94d0bac94a48cc4f3f3e1a
satellitex <s.a.t.e.3.ths@gmail.com>
2019-05-13 14:57:26 +0900

    WIP: impl cli tools

---
+++ packages/cli/src/commands.js
@@ -0,0 +1,11 @@
+exports.deposit = () => {
+    console.log('deposit!!')
+}
+
+exports.exit = () => {
+    console.log('exit!!')
+}
+
+exports.transfer = () => {
+    console.log('transfer!!')
+}
--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,28 +1,60 @@
-#!/usr/bin/env node
-require('shelljs/global')
+const { AutoComplete, Form } = require('enquirer');
+const {deposit, exit, transfer } =  require('./commands');
 
-program = require('commander')
-prompt = require('co-prompt')
-co = require('co')
 
-program
-  .arguments('<file>')
-  .option('-u, --username <username>', 'The user to authenticate as')
-  .option('-p, --password <password>', 'The user\'s password')
-  .action(function(file) {
-    co(function *() {
-      var username, password
-      if (program.username) {
-        username = program.username
-      } else {
-        username = yield prompt('username: ')
+var default_p_endpoint = ''
+var default_c_endpoint = ''
+if (process.env.PARENT_CHAIN_ENDPOINT) {
+  default_p_endpoint = process.env.PARENT_CHAIN_ENDPOINT
+}
+if (process.env.CHILD_CHAIN_ENDPOINT) {
+  default_c_endpoint = process.env.CHILD_CHAIN_ENDPOINT
+}
+
+const prompt = new Form({
+  name: 'endpoints',
+  message: 'Please endpoints the following information:',
+  choices: [
+    { name: 'parent_endpoint', message: 'Parent Endpoint', initial: default_p_endpoint },
+    { name: 'child_endpoint', message: 'Child Endpoint', initial: default_c_endpoint },
+  ]
+});
+
+prompt.run()
+  .then(value => start(value.parent_endpoint, value.child_endpoint))
+  .catch(console.error);
+
+const start = (parent, child) => {
+  console.log(`${parent}, ${child}`);
+  const main_prompt = new AutoComplete({
+    name: 'command',
+    message: 'What do you do now?',
+    limit: 3,
+    choices: [
+      'transfer',
+      'deposit',
+      'exit',
+      'set_owner',
+    ]
+  });
+  main_prompt.run()
+    .then(answer => {
+      console.log('Command:', answer)
+      switch (answer) {
+        case 'transfer':
+          transfer()
+          break;
+        case 'deposit':
+          deposit()
+          break;
+        case 'exit':
+          exit()
+          break;
+        default:
+          console.log('unimplemented command.');
+          break;
       }
-      password = yield prompt.password('password: ')
-      display(username, password, file)
+      start(parent, child)
     })
-  })
-  .parse(process.argv)
-
-display = (username, password, file) => {
-  echo(`user: ${username} pass: ${password} file: ${file}`)
+    .catch(console.error);  
 }
