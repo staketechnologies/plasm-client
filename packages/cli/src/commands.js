351b80bee5f87c8cbf34c9537c92c1e3ad3df29f
satellitex <s.a.t.e.3.ths@gmail.com>
2019-05-15 13:54:01 +0900

    impl get methods

--- packages/cli/src/commands.js
+++ packages/cli/src/commands.js
@@ -1,43 +1,84 @@
 const { Input, NumberPrompt } = require('enquirer');
+const { KeyGenerator, genTransfer, getUtxoList, getBalance } = require('@plasm/util');
 
 async function getSender(owner) {
-    if(owner) {} else {
-        owner = '';
-    }
-    const prompt = new Input({
-        message: 'What is tx sender name?',
-        initial: owner
-      });
-    answer = await prompt.run()
-    return answer
+  if(owner) {} else {
+    owner = '';
+  }
+  const prompt = new Input({
+    message: 'What is tx sender name?',
+    initial: owner
+  });
+  answer = await prompt.run()
+  return answer
 }
 
 exports.deposit = async function(api, owner) {
-    owner = await getSender(owner);
+  owner = await getSender(owner);
 
-    const prompt = new NumberPrompt({
-        name: 'value',
-        message: 'deposit value'
-      });      
-    value = await prompt.run()
-    
+  const prompt = new NumberPrompt({
+      name: 'value',
+      message: 'deposit value'
+    });
+  value = await prompt.run()
+
+  // generate keypair form `${owner}`.
+  const keyPair = KeyGenerator.instance.from(owner);
+  const hash = await api.tx.parentMvp
+    .deposit(value)
+    .signAndSend(keyPair);
+  console.error('Success deposited!: ', hash.toHex());
 }
 
 exports.exit = async function(api, owner) {
-    owner = await getSender(owner);
-    console.log('exit!!')
+  owner = await getSender(owner);
+  console.log('exit!!')
 }
 
 exports.transfer = async function(api, owner) {
-    owner = await getSender(owner);
-    console.log('transfer!!')
+  owner = await getSender(owner);
+  console.log('transfer!!')
+
+  const prompt = new Input({
+      message: 'What name is transfer to?'
+  })
+  const dest = await prompt.run();
+
+  prompt = new NumberPrompt({
+      name: 'value',
+      message: 'deposit value'
+    });
+  value = await prompt.run()
+
+  const keyPair = KeyGenerator.instance.from(owner);
+  const tx = genTransfer(api, keyPair, keyPair.address(), dest, value, 100);
+  const hash = await api.tx.utxoMvp
+    .execute(tx)
+    .signAndSend(keyPair);
+  console.log('Success Tx!: ', hash.toHex())
 }
 
 exports.setOwner = async function() {
-    const prompt = new Input({
-        message: 'What is tx sender name?'
-      });
-      
-    answer = await prompt.run()
-    return answer
+  const prompt = new Input({
+      message: 'What is tx sender name?'
+    });
+    
+  answer = await prompt.run()
+  return answer
+}
+
+exports.displayBalance = async function(api, owner) {
+  owner = await getSender(owner);
+  const keyPair = KeyGenerator.instance.from(owner);
+  const balance = await api.query.balances.freeBalance(keyPair.address());
+  console.log(`${owner} has parent balance: ${balance}.`);
+}
+
+exports.displayUtxo = async function(api, owner) {
+  owner = await getSender(owner);
+  const keyPair = KeyGenerator.instance.from(owner);
+  const utxoList = await getUtxoList(api, keyPair.address())
+  console.log(`${owner} has child utxoList: ${utxoList}`);
+  const balance = await getBalance(api, keyPair.address());
+  console.log(`Sum of child balance: ${balance}`);
 }
--- packages/cli/src/index.js
+++ packages/cli/src/index.js
@@ -1,5 +1,5 @@
 const { AutoComplete, Form } = require('enquirer');
-const {deposit, exit, transfer, setOwner} =  require('./commands');
+const {deposit, exit, transfer, setOwner, displayBalance, displayUtxo} =  require('./commands');
 const { create } = require('@plasm/util')
 
 var default_p_endpoint = ''
@@ -20,48 +20,62 @@ const prompt = new Form({
   ]
 });
 
-prompt.run()
-  .then(async value => {
-    parent = await create(value.parent_endpoint);
-    child = await create(value.child_endpoint);
-    await start(parent, child, null)
-  })
-  .catch(console.error);
-
-const start = async (parent, child, owner) => {
-  console.log(`${parent}, ${child}, ${owner}`);
-  const main_prompt = new AutoComplete({
-    name: 'command',
-    message: 'What do you do now?',
-    limit: 3,
-    choices: [
-      'transfer',
-      'deposit',
-      'exit',
-      'set_owner',
-    ]
-  });
-  main_prompt.run()
-    .then(async answer => {
-      console.log('Command:', answer)
-      switch (answer) {
-        case 'transfer':
-          await transfer(child, owner)
-          break;
-        case 'deposit':
-          await deposit(parent, owner)
-          break;
-        case 'exit':
-          await exit(parent, owner)
-          break;
-        case 'set_owner':
-          owner = await setOwner()
-          break;
-        default:
-          console.log('unimplemented command.');
-          break;
-      }
-      start(parent, child, owner)
+async function main() {
+  prompt.run()
+    .then(async value => {
+      parent = await create(value.parent_endpoint);
+      child = await create(value.child_endpoint);
+      await start(parent, child, null)
     })
-    .catch(console.error);  
+    .catch(console.error);
+
+  const start = async (parent, child, owner) => {
+    console.log(`${parent}, ${child}, ${owner}`);
+    const main_prompt = new AutoComplete({
+      name: 'command',
+      message: 'What do you do now?',
+      limit: 10,
+      choices: [
+        'transfer',
+        'deposit',
+        'exit',
+        'set_owner',
+        'balance',
+        'utxo'
+      ]
+    });
+    main_prompt.run()
+      .then(async answer => {
+        console.log('Command:', answer)
+        switch (answer) {
+          case 'transfer':
+            await transfer(child, owner)
+            break;
+          case 'deposit':
+            await deposit(parent, owner)
+            break;
+          case 'exit':
+            await exit(parent, owner)
+            break;
+          case 'set_owner':
+            owner = await setOwner()
+            break;
+          case 'balance':
+            await displayBalance(parent, owner)
+            break;
+          case 'utxo':
+            await displayUtxo(child, owner)
+            break;  
+          default:
+            console.log('unimplemented command.');
+            break;
+        }
+        start(parent, child, owner)
+      })
+      .catch(console.error);  
+  }
 }
+
+main()
+  .then(() => console.log('Run the Plasm CLI') )
+  .catch(e=> console.error(e) )
